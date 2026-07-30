5fdf41620c9fafc5d0937d0b9d262a3a5c0cedc9
Yinhan-Lu <143548426+Yinhan-Lu@users.noreply.github.com>
2026-07-30 05:23:53 +0000

    Deploying to gh-pages from @ Yinhan-Lu/yinhan-website@f6a2d8a1af533322a270b08f58754ec805b7f52f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -67,6 +67,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/yinhan-lu", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=PgLzinEAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
